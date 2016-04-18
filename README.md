4e3505f47c00791a52e8ff3e8fcda048884a02f0
Kenichiro MATOHARA <matoken@gmail.com>
2016-04-19 00:26:09 +0900

    add comment

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rss2tw
+
+ask.libreoffice.orgのRSSから最新5分以内のtitle/urlを表示  
+crontabで5分毎に実行してtw.plやttytterに繋ぐのを想定
+
+```
+$ ./rss2tw.pl
+IFTTTによるTwitter自動投稿の試験 http://ask.libreoffice.org/ja/question/68505/iftttniyorutwitterzi-dong-tou-gao-noshi-yan/
+$ ./rss2tw.pl | tw
+```
+
+
--- rss2tw.pl
+++ rss2tw.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl
 
-#-- ask.libreoffice.orgのRSSから最新5分以内のtitle/urlを表示
+# ask.libreoffice.orgのRSSから最新5分以内のtitle/urlを表示
 # crontabで5分毎に実行してtw.plやttytterに繋ぐのを想定
-# 
+#
+#   $ ./rss2tw.pl
+#   IFTTTによるTwitter自動投稿の試験 http://ask.libreoffice.org/ja/question/68505/iftttniyorutwitterzi-dong-tou-gao-noshi-yan/
+#   $ ./rss2tw.pl | tw
+#
 # KenichiroMATOHARA <matoken@gmail.com>
 
 use strict;
