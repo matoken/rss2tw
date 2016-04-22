48e4c0c945652639608bb33c00ed8963129338e2
Kenichiro MATOHARA <matoken@gmail.com>
2016-04-22 18:23:19 +0900

    rss解析をXML::RSSLite に変更

--- rss2tw.pl
+++ rss2tw.pl
@@ -11,9 +11,38 @@
 
 use strict;
 use warnings;
+use Encode;
+use LWP::Simple;
+use XML::RSSLite;
+
+my $RSS = 'https://ask.libreoffice.org/ja/feeds/rss/';
+
+my $content = get( $RSS );
+die "Couldn't get it!" unless defined $content;
+
+my %result;
+parseRSS(\%result, \$content);
+
+foreach my $item (@{$result{'item'}}) {
+print "  --- Item ---\n",
+  "  Title: $item->{'title'}\n",
+#  "  Desc:  $item->{'description'}\n",
+  "  Link:  $item->{'link'}\n",
+  "  pubDate:  $item->{'pubDate'}\n\n";
+
+  my $date  = `printf \`date --date='$item->{'pubDate'}' +%s\``;
+  my $now_time = `printf \`date +%s\``;
+  if($date > ($now_time-600000) ){
+    print Encode::encode('utf8', $item->{'title'})," $item->{'link'}";
+#     print "$item->{'title'} $item->{'link'}\n";
+  }else{ exit }
+}
+
+
+__DATA__
 
 my ($title, $link)= ('', '');
-foreach my $line ( `curl -s -S https://ask.libreoffice.org/ja/feeds/rss/ | xmllint --format - | egrep '<title>|<link>|<pubDate>' | tail -n +3 | cut -c7-` ){
+foreach my $line ( `curl -s -S $RSS | xmllint --format - | egrep '<title>|<link>|<pubDate>' | tail -n +3 | cut -c7-` ){
   chomp $line;
   if(    $line =~/\<title\>/   ){ $title = $line; $title =~s/<[^>]*>//g; }
   elsif( $line =~/\<link\>/    ){ $link  = $line; $link  =~s/<[^>]*>//g; }
